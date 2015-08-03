6c173f990e504af9804fe115d5e8d8693729935d
Christopher Carlson <christopher.s.carlson@wmich.edu>
2015-08-03 16:58:46 -0400

    Created Basic Record Structure migration.

--- db/migrate/20150803174222_create_employees.rb
+++ db/migrate/20150803174222_create_employees.rb
@@ -1,17 +1,21 @@
 class CreateEmployees < ActiveRecord::Migration
   def change
     create_table :employees do |t|
-      t.string :first_name
-      t.name :last
-      t.string :business_email
-      t.string :personal_email
-      t.string :cell_phone
-      t.string :business_phone
-      t.string :address
-      t.string :department
-      t.string :office
+      t.string :first_name, null: false, limit: 30
+      t.string :last_name, null: false, limit: 30
+      t.string :business_email, null: false, :unique => true, limit: 70
+      t.string :personal_email, :default => 'N/A', limit: 70
+      t.string :cell_phone, :default => 'N/A', limit: 14
+      t.string :business_phone, :null => false, limit: 14
+      t.string :phone_extension, :default => 'N/A', limit: 5
+      t.string :address, default: 'N/A'
+      t.string :department, null: false
+      t.string :office_number, null:false
 
       t.timestamps null: false
     end
+    add_index :employees, :first_name
+    add_index :employees, :last_name
+    add_index :employees, :department
   end
 end
---
+++ db/schema.rb
@@ -0,0 +1,35 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20150803174222) do
+
+  create_table "employees", force: :cascade do |t|
+    t.string   "first_name",      limit: 30,                 null: false
+    t.string   "last_name",       limit: 30,                 null: false
+    t.string   "business_email",  limit: 70,                 null: false
+    t.string   "personal_email",  limit: 70, default: "N/A"
+    t.string   "cell_phone",      limit: 14, default: "N/A"
+    t.string   "business_phone",  limit: 14,                 null: false
+    t.string   "phone_extension", limit: 5,  default: "N/A"
+    t.string   "address",                    default: "N/A"
+    t.string   "department",                                 null: false
+    t.string   "office_number",                              null: false
+    t.datetime "created_at",                                 null: false
+    t.datetime "updated_at",                                 null: false
+  end
+
+  add_index "employees", ["department"], name: "index_employees_on_department"
+  add_index "employees", ["first_name"], name: "index_employees_on_first_name"
+  add_index "employees", ["last_name"], name: "index_employees_on_last_name"
+
+end
