e89143382b141b79bbe73bffc41f1d96f9f5422e
Christopher Carlson <christopher.s.carlson@wmich.edu>
2015-08-05 17:51:29 -0400

    Rearranged the data on forms etc to fit the database schema.

--- db/migrate/20150803174222_create_employees.rb
+++ db/migrate/20150803174222_create_employees.rb
@@ -1,23 +1,29 @@
 class CreateEmployees < ActiveRecord::Migration
   def change
     create_table :employees do |t|
-      t.string :first_name, null: false, limit: 30
-      t.string :last_name, null: false, limit: 30
-      t.string :business_email, null: false, :unique => true, limit: 70
-      t.string :personal_email, :default => 'N/A', limit: 70
-      t.string :cell_phone, :default => 'N/A', limit: 14
-      t.string :business_phone, :null => false, limit: 14
-      t.string :phone_extension, :default => 'N/A', limit: 5
-      t.string :address, default: 'N/A'
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :business_email, null: false, :unique => true
+      t.string :personal_email, :default => 'N/A'
+      t.string :cell_phone, :default => 'N/A'
+      t.string :business_phone, :null => false
+      t.string :extension, :default => 'N/A'
+      t.string :home_phone, default: 'N/A'
       t.string :department, default: 'N/A'
       t.string :office_number, default: 'N/A'
       t.string :location, default: 'N/A'
       t.string :notes
+
+      t.integer :manager_id
+
+      t.string :password_digest
+      t.string :salt
 
       t.timestamps null: false
     end
     add_index :employees, :first_name
     add_index :employees, :last_name
     add_index :employees, :department
+    add_index :employees, :business_email
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,22 +14,26 @@
 ActiveRecord::Schema.define(version: 20150803174222) do
 
   create_table "employees", force: :cascade do |t|
-    t.string   "first_name",      limit: 30,                 null: false
-    t.string   "last_name",       limit: 30,                 null: false
-    t.string   "business_email",  limit: 70,                 null: false
-    t.string   "personal_email",  limit: 70, default: "N/A"
-    t.string   "cell_phone",      limit: 14, default: "N/A"
-    t.string   "business_phone",  limit: 14,                 null: false
-    t.string   "phone_extension", limit: 5,  default: "N/A"
-    t.string   "address",                    default: "N/A"
-    t.string   "department",                 default: "N/A"
-    t.string   "office_number",              default: "N/A"
-    t.string   "location",                   default: "N/A"
+    t.string   "first_name",                      null: false
+    t.string   "last_name",                       null: false
+    t.string   "business_email",                  null: false
+    t.string   "personal_email",  default: "N/A"
+    t.string   "cell_phone",      default: "N/A"
+    t.string   "business_phone",                  null: false
+    t.string   "extension",       default: "N/A"
+    t.string   "home_phone",      default: "N/A"
+    t.string   "department",      default: "N/A"
+    t.string   "office_number",   default: "N/A"
+    t.string   "location",        default: "N/A"
     t.string   "notes"
-    t.datetime "created_at",                                 null: false
-    t.datetime "updated_at",                                 null: false
+    t.integer  "manager_id"
+    t.string   "password_digest"
+    t.string   "salt"
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
   end
 
+  add_index "employees", ["business_email"], name: "index_employees_on_business_email"
   add_index "employees", ["department"], name: "index_employees_on_department"
   add_index "employees", ["first_name"], name: "index_employees_on_first_name"
   add_index "employees", ["last_name"], name: "index_employees_on_last_name"
