b985f70b038f151ad6473405984705262afd085b
Christopher Carlson <christopher.s.carlson@wmich.edu>
2015-08-12 20:24:12 -0400

    Added a rake file to automate the whole db:drop db:create db:migrate db:seed procedure.  Now rake hard_reset_db will do all these things automatically.  Also improved search to include a department filter which automatically filters results by department.  Also, passing an empty string and a department will return all employees from that department.

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -7,6 +7,7 @@ class Employee < ActiveRecord::Base
   validates_presence_of :business_email
   validates_uniqueness_of :business_email
 
+  scope :dept, ->(department) {where :department => department}
 
 def self.authenticate(email, password)
   employee = Employee.find_by business_email: email
@@ -23,23 +24,33 @@ def full_name
 end
 
 def self.search(search = nil, dept_filter = nil)
-  if search
-    search_strings = search.split
-    search_strings2 = search_strings.collect {|s| s.strip}
-    if search_strings2.count == 1
-      @employees = Employee.where('first_name LIKE ? OR last_name LIKE ? OR business_email LIKE ? OR department LIKE ?', "%#{search_strings[0]}%","%#{search_strings[0]}%","%#{search_strings[0]}%","%#{search_strings[0]}%")
+  if !search.blank?
+    search_strings = search.split #Break the search string up into individual terms.
+    search_strings2 = search_strings.collect {|s| s.strip} #Remove whitespace
+    if search_strings2.count == 1 #Don't do extra processing if only one string being searched.
+      @employees = Employee.where('first_name LIKE ? OR last_name LIKE ? OR business_email LIKE ?', "%#{search_strings[0]}%","%#{search_strings[0]}%","%#{search_strings[0]}%")
     else
-      emps = search_strings2.collect { |str| Employee.where('first_name LIKE ? OR last_name LIKE ? OR business_email LIKE ? OR department LIKE ?', "%#{str}%","%#{str}%","%#{str}%","%#{str}%")}
+      emps = search_strings2.collect { |str| Employee.where('first_name LIKE ? OR last_name LIKE ? OR business_email LIKE ?', "%#{str}%","%#{str}%","%#{str}%")}
 
-      emps2 = emps.reject{|e| e.blank?}
-      @employees = emps2.reduce(:&)
-
-      @employees ||= []
-
+      emps2 = emps.reject{|e| e.blank?} #Remove any search strings that returned
+                                        #an empty set.
+      @employees = emps2.reduce(:&) #Intersect each set of results that returned
+                                    #something and save that to employees variable.
     end
+  elsif dept_filter
+    @employees = Employee.dept(dept_filter)
   else
     @employees = []
   end
+
+  if dept_filter && !@employees.nil?
+    @employees = @employees.where(department: dept_filter)
+  end
+  @employees ||= []     #Incase the result search with department is nil
+end
+
+def self.unique_departments
+  Employee.uniq.pluck(:department)
 end
 
 private
